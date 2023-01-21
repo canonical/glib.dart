425cb3c2cef0c851e08be23d908b777a0f03ff55
J-P Nurmi <jpnurmi@gmail.com>
2023-01-21 16:50:30 +0100

    Resolve .dylib on macOS

--- lib/src/ffi/libglib.dart
+++ lib/src/ffi/libglib.dart
@@ -1,4 +1,5 @@
 import 'dart:ffi' as ffi;
+import 'dart:io' as io;
 
 import 'package:meta/meta.dart';
 
@@ -6,8 +7,13 @@ import 'libglib.g.dart';
 export 'libglib.g.dart';
 
 LibGLib? _lib;
-LibGLib get lib =>
-    _lib ??= LibGLib(ffi.DynamicLibrary.open('libglib-2.0.so.0'));
+LibGLib get lib => _lib ??= LibGLib(ffi.DynamicLibrary.open(_env ?? _platform));
+
+String? get _env => io.Platform.environment['LIBGLIB_PATH'];
+String get _platform {
+  if (io.Platform.isMacOS) return 'libglib-2.0.0.dylib';
+  return 'libglib-2.0.so.0';
+}
 
 @visibleForTesting
 void overrideLibGLibForTesting(LibGLib lib) => _lib = lib;
