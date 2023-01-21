39db73f421e4bb1255f44c81a9d3f3f9d66c5b88
J-P Nurmi <jpnurmi@gmail.com>
2023-01-21 18:50:32 +0100

    Add Windows support

--- lib/src/ffi/libglib.dart
+++ lib/src/ffi/libglib.dart
@@ -12,6 +12,7 @@ LibGLib get lib => _lib ??= LibGLib(ffi.DynamicLibrary.open(_env ?? _platform));
 String? get _env => io.Platform.environment['LIBGLIB_PATH'];
 String get _platform {
   if (io.Platform.isMacOS) return 'libglib-2.0.0.dylib';
+  if (io.Platform.isWindows) return 'glib-2.0-0.dll';
   return 'libglib-2.0.so.0';
 }
 
