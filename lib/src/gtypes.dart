c1efbbc4ed92acc05953309826cec1d342c2c07f
J-P Nurmi <jp.nurmi@canonical.com>
2023-01-22 16:23:53 +0100

    Add more typedefs

--- lib/src/gtypes.dart
+++ lib/src/gtypes.dart
@@ -9,10 +9,20 @@ typedef gushort = int;
 typedef gulong = int;
 typedef guint = int;
 
+typedef gint8 = int;
+typedef guint8 = int;
+typedef gint16 = int;
+typedef guint16 = int;
 typedef gint32 = int;
 typedef guint32 = int;
 typedef gint64 = int;
 typedef guint64 = int; // :(
+
+typedef gssize = int;
+typedef gsize = int;
+typedef goffset = int;
+typedef gintptr = int;
+typedef guintptr = int;
 
 typedef gfloat = double;
 typedef gdouble = double;
